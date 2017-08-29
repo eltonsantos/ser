327448fe41042d1894ce4aca54899697f7a6af90
Elton Santos <eltonaxl@hotmail.com>
2017-08-29 09:26:04 -0300

    Create tables of the system

---
+++ src/main/java/br/com/cagece/model/TipoRede.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cagece.model;
+
+/**
+ *
+ * @author 211905
+ */
+public class TipoRede {
+    
+    private Integer id;
+    private String descricao;
+    private String observacao;
+    private Enum produto;
+    
+    
+}
