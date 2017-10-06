1e65c9d95f684a8712b97a5bdbb84d3e2d134c97
Elton Santos <eltonaxl@hotmail.com>
2017-10-06 17:48:10 -0300

    Change to postgres

--- src/main/java/br/com/cagece/model/Diametro.java
+++ src/main/java/br/com/cagece/model/Diametro.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.cagece.model;
 
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
